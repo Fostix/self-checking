6c4f206e75c52911fd653d83f195aac79d46792b
Albert <accretiondiskthat@gmail.com>
2022-12-20 13:49:31 +0500

    all broken

---
+++ src/test/java/com/selfcheck/selfcheckingComponent/controller/query/QueriesTest.java
@@ -0,0 +1,33 @@
+package com.selfcheck.selfcheckingComponent.controller.query;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class QueriesTest {
+
+    @Test
+    void send() {
+    }
+
+    @Test
+    void post() {
+    }
+
+    @Test
+    void testPost() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void put() {
+    }
+}
